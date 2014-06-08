b0c1600d2cbf4f62bf064f93b2e3ea62980b11ba
Guillaume Aubert <aubertg@cpan.org>
2014-06-08 12:12:45 -0700

    Release version 1.0.2.

--- lib/App/GitHooks/Plugin/DetectCommitNoVerify.pm
+++ lib/App/GitHooks/Plugin/DetectCommitNoVerify.pm
@@ -29,11 +29,11 @@ will add their output to the commit message for posterity (and public shaming).
 
 =head1 VERSION
 
-Version 1.0.1
+Version 1.0.2
 
 =cut
 
-our $VERSION = '1.0.1';
+our $VERSION = '1.0.2';
 
 
 =head1 METHODS
